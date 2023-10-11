c51a85c77bbe6aff314caf2252f3b1680d6dad28
Eduardo Gonzalez <eduardogl15.personal@gmail.com>
2023-10-11 11:58:37 -0300

    Agregué nuevas pruebas de inserción y eliminación

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -3,14 +3,16 @@
 #include "src/abb_estructura_privada.h"
 
 int comparador(void* e1, void* e2){
-	if(e1 == NULL || e2 == NULL)
-		return 0;
+	int valor_1 = *(int*)e1;
+	int valor_2 = *(int*)e2;
 
-	if(e1 == e2)
+	if(valor_1 < valor_2)
+		return -1;
+	else if(valor_1 > valor_2)
 		return 1;
 	else	
 		return 0;
-}
+	}
 
 void pruebas_creacion(){
 	abb_t* arbol = abb_crear(comparador);
@@ -18,34 +20,60 @@ void pruebas_creacion(){
 	pa2m_afirmar(arbol->nodo_raiz == NULL, "El nodo raiz es nulo.");
 	pa2m_afirmar(abb_vacio(arbol) == true, "El arbol se encuentra vacío.");
 	pa2m_afirmar(abb_tamanio(arbol) == 0, "El arbol tiene tamaño cero.");
-	
+		
 	abb_destruir(arbol);
 }
 
 void pruebas_de_insercion(){
 	abb_t* arbol = abb_crear(comparador);
-	int numeros[] = {5,3,4};
-	for(int i = 0; i < 3; i++){
-		abb_insertar(arbol, &numeros[i]);
-	}
-	
-	pa2m_afirmar(abb_vacio(arbol) == false, "El arbol no se encuentra vacío.");
-	pa2m_afirmar(abb_tamanio(arbol) == 3, "El arbol tiene tamaño 3.");
-	//pa2m_afirmar(*(int*)arbol->nodo_raiz->elemento == 5, "El primer elemento insertado queda como raiz ");
-	//pa2m_afirmar(*(int*)arbol->nodo_raiz->izquierda->elemento == 3, "El hijo izquierdo es el esperado.");
-	//pa2m_afirmar(*(int*)arbol->nodo_raiz->derecha->elemento == 4, "El hijo derecho es el esperado.");
+	int numeros[] = {5,3,6};
+		
+    pa2m_afirmar(abb_insertar(arbol, &numeros[0]) == arbol, "Puedo insertar un elemento en el abb.");
+    pa2m_afirmar(arbol->nodo_raiz->elemento == &numeros[0], "El elemento queda como valor raíz");
+    pa2m_afirmar(abb_vacio(arbol) == false, "El abb ya no está vacío.");
+    pa2m_afirmar(abb_tamanio(arbol) == 1, "La cantidad de elementos es 1");
 
+	pa2m_afirmar(abb_insertar(arbol, &numeros[1]) == arbol, "Puedo insertar un nuevo elemento en el abb.");
+    pa2m_afirmar(arbol->nodo_raiz->izquierda != NULL && arbol->nodo_raiz->derecha == NULL, "El abb queda con un hijo izquierdo y sin hijo derecho.");
+    pa2m_afirmar(arbol->comparador(arbol->nodo_raiz->izquierda->elemento, arbol->nodo_raiz->elemento) < 0, "El elemento insertado queda a la izquierda de la raíz.");
+    pa2m_afirmar(abb_tamanio(arbol) == 2, "La cantidad de elementos es 2");
+    
+    pa2m_afirmar(abb_insertar(arbol, &numeros[2]) == arbol, "Puedo insertar un elemento mayor que el que se encuentra en la raiz.");
+    pa2m_afirmar(arbol->nodo_raiz->izquierda != NULL && arbol->nodo_raiz->derecha != NULL, "El abb queda con un hijo izquierdo y un hijo derecho.");
+    pa2m_afirmar(abb_tamanio(arbol) == 3, "La cantidad de elementos es 3");
+  
 	abb_destruir(arbol);
-}
+	}
 
 void pruebas_de_eliminacion(){
 	abb_t* arbol = abb_crear(comparador);
-	int numeros[] = {5,3,4};
-	for(int i = 0; i < 3; i++){
+	int numeros[] = {5,3,4,6};
+	for(int i = 0; i < 4; i++){
 		abb_insertar(arbol, &numeros[i]);
 	}
+	pa2m_afirmar(abb_tamanio(arbol) == 4, "La cantidad de elementos es 4.");
 
-	pa2m_afirmar(abb_quitar(arbol, &numeros[0]) == &numeros[0], "Se pudo extraer un elemento");
+    pa2m_afirmar(abb_quitar(arbol, &numeros[0]) == &numeros[0], "Puedo borrar un nodo raiz");
+	pa2m_afirmar(abb_buscar(arbol, &numeros[0]) == NULL, "El nodo eliminado no existe más.");
+	pa2m_afirmar(abb_tamanio(arbol) == 3, "La cantidad de elementos es 3.");
+
+	pa2m_afirmar(abb_quitar(arbol, &numeros[0]) == NULL, "No puedo borrar un elemento que no existe en el arbol");
+	pa2m_afirmar(abb_tamanio(arbol) == 3, "La cantidad de elementos sigue siendo 3.");
+
+    pa2m_afirmar(abb_quitar(arbol, &numeros[3]) == &numeros[3], "Puedo borrar un nodo hoja.");
+    pa2m_afirmar(abb_buscar(arbol, &numeros[3]) == NULL, "El nodo eliminado no existe más.");
+	pa2m_afirmar(abb_tamanio(arbol) == 2, "La cantidad de elementos es 2.");
+
+	pa2m_afirmar(abb_quitar(arbol, &numeros[1]) == &numeros[1], "Puedo borrar un nodo hoja");
+	pa2m_afirmar(abb_buscar(arbol, &numeros[1]) == NULL, "El nodo eliminado no existe más.");
+	pa2m_afirmar(abb_tamanio(arbol) == 1, "La cantidad de elementos es 1.");
+
+	pa2m_afirmar(abb_quitar(arbol, &numeros[2]) == &numeros[2], "Puedo borrar el ultimo nodo que queda");
+    pa2m_afirmar(abb_buscar(arbol, &numeros[2]) == NULL, "El nodo eliminado no existe más.");
+    pa2m_afirmar(abb_tamanio(arbol) == 0, "La cantidad de elementos es 0.");
+    pa2m_afirmar(abb_vacio(arbol) == true, "El arbol está vacio.");
+
+	pa2m_afirmar(abb_quitar(arbol, &numeros[2]) == NULL, "No puedo borrar un elemento en un arbol vacio");
 
 	abb_destruir(arbol);
 
